79b442c7cb8b072aa6f9aa7068cb3d61bdfb8dda
Ricardo Altmann <ricardoaltmann@outlook.com.br>
2021-11-13 17:49:30 -0300

    ajustando timezone

--- date.php
+++ date.php
@@ -1,10 +1,13 @@
 <?php
 
-$agora = new DateTime();
-echo $agora->format('d/m/y H:i');
+$timezone = new DateTimeZone('America/Sao_Paulo');
+$agora = new DateTime('now',$timezone);
+echo $agora->format('d/m/y H:i') . PHP_EOL;
 
 
 $entrada = new DateTime('09:00');
 $saida = new DateTime('18:00');
 $intervalo = $saida->diff($entrada);
 print_r($intervalo);
+
+echo $intervalo->h;
