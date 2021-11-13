3e475028390353c0ccab1e2e3105b56e08223993
Ricardo Altmann <ricardoaltmann@outlook.com.br>
2021-11-13 17:39:45 -0300

    diff de entrada e saida

--- date.php
+++ date.php
@@ -2,3 +2,9 @@
 
 $agora = new DateTime();
 echo $agora->format('d/m/y H:i');
+
+
+$entrada = new DateTime('09:00');
+$saida = new DateTime('18:00');
+$intervalo = $saida->diff($entrada);
+print_r($intervalo);
